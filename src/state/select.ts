e48c3c99be0a1a60579c192ce833f0d65b06ee15
Mark Lawrence <mark.lawrence@tandem.co.uk>
2022-01-23 09:51:56 +0000

    feat: success and failure selectors

--- src/state/select.ts
+++ src/state/select.ts
@@ -1,6 +1,7 @@
 import { AppState, GUESSES } from "../model";
 
-export const isGameOver = ({ guesses, target }: AppState) =>
-  guesses.length >= GUESSES
-    ? true
-    : guesses.length > 0 && guesses[guesses.length - 1] === target;
+export const isSuccess = ({ guesses, target }: AppState): boolean => guesses.length > 0 && guesses[guesses.length - 1] === target
+
+export const isFail = ({ guesses }: AppState): boolean => guesses.length >= GUESSES
+
+export const isGameOver = (state: AppState): boolean => isSuccess(state) || isFail(state)
